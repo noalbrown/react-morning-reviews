e02f0b06eaff791fa2d6c253396007591c3669c5
ackentdev <adamkentdev@gmail.com>
2020-07-23 09:15:15 -0700

    good morning wr3!

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            year: '',
+            posterImg: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render(){
+        const {title, year, posterImg} = this.state;
+        return(
+        <form className="Form">
+            <input 
+                name="title" 
+                value={title} 
+                placeholder="Title"
+                onChange={this.handleChange}/>
+            <input 
+                name="year" 
+                value={year} 
+                placeholder="Year"
+                onChange={this.handleChange}/>
+            <input 
+                name="posterImg" 
+                value={posterImg} 
+                placeholder="Poster url"
+                onChange={this.handleChange}/>
+            <button type="submit">Add Movie</button>
+        </form>
+    )}
+}
+
+export default Form
--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import movies from '../data/movies.json';
+import ListItem from './ListItem';
+import Form from './Form';
 
 class List extends Component {
   constructor() {
     super()
-
     this.state = {
-      movies: [],
+      movies: movies
     }
   }
 
+
   render() {
-    return <div>I am the list component</div>
+    const moviesMap = this.state.movies.map( elem => {
+      return <ListItem key={elem.id} movie={elem}/>
+    })
+  return <div className="List">
+      <Form/>
+      {moviesMap}
+    </div>
   }
 }
 export default List
