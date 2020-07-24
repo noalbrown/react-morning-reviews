1dc8b840816c63bddb68a754de5f8b7c27c481f8
Noal Brown <noalbrown1@gmail.com>
2020-07-24 11:05:18 -0500

    react

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Form extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             title: '',
@@ -9,36 +9,44 @@ class Form extends Component {
             posterImg: ''
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
     }
 
-    handleChange(e){
+    handleChange(e) {
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
-    render(){
-        const {title, year, posterImg} = this.state;
-        return(
-        <form className="Form">
-            <input 
-                name="title" 
-                value={title} 
-                placeholder="Title"
-                onChange={this.handleChange}/>
-            <input 
-                name="year" 
-                value={year} 
-                placeholder="Year"
-                onChange={this.handleChange}/>
-            <input 
-                name="posterImg" 
-                value={posterImg} 
-                placeholder="Poster url"
-                onChange={this.handleChange}/>
-            <button type="submit">Add Movie</button>
-        </form>
-    )}
+    handleAdd(event) {
+        const { title, year, posterImg } = this.state
+        event.preventDefault()
+        this.props.addMovie(title, year, posterImg)
+    }
+
+    render() {
+        const { title, year, posterImg } = this.state;
+        return (
+            <form className="Form" onSubmit={this.handleAdd}>
+                <input
+                    name="title"
+                    value={title}
+                    placeholder="Title"
+                    onChange={this.handleChange} />
+                <input
+                    name="year"
+                    value={year}
+                    placeholder="Year"
+                    onChange={this.handleChange} />
+                <input
+                    name="posterImg"
+                    value={posterImg}
+                    placeholder="Poster url"
+                    onChange={this.handleChange} />
+                <button type="submit">Add Movie</button>
+            </form>
+        )
+    }
 }
 
 export default Form
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ListItem = ({movie}) => {
-    // const {movie} = props;
+const ListItem = (props) => {
+    const { movie, deleteMovie } = props;
     return <div className="List-Item">
-        <img alt={movie.title} className="movie-poster" src={movie.posterImg}/>
+        <img alt={movie.title} className="movie-poster" src={movie.posterImg} />
         <div className="movie-info">
             <p>{movie.title}</p>
             <p>{movie.year}</p>
